1a68f878f81fe5495d0234bcd4880ec0a9026e3a
GoHippo <goodsonycompact@gmail.com>
2024-08-07 00:00:23 +0300

    add fatal and panic

--- sl/tools.go
+++ sl/tools.go
@@ -1,10 +1,23 @@
 package sl
 
-import "log/slog"
+import (
+	"log"
+	"log/slog"
+	"os"
+)
 
 func Err(err error) slog.Attr {
 	return slog.Attr{
 		Key:   "error",
 		Value: slog.StringValue(err.Error()),
 	}
+}
+
+func Fatal(logger *slog.Logger, err error) {
+	logger.Error("Fatal error:", Err(err))
+	os.Exit(1)
+}
+
+func Panic(logger *slog.Logger, err error) {
+	log.Panic("Panic error:" + err.Error())
 }
